7f5f95a260421cc3512821343c33f237e8601760
Mateus Maranhão <feradourada@gmail.com>
2023-08-20 10:24:23 -0300

    Implement Cadastro.vue and Login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="app-view">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-  .app-view {
-    background-color: rgb(0, 0, 0);
-    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-  }
-</style>
+.app-view {
+  background-color: rgb(22, 22, 22);
+  /* Cor cinza */
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+body::-webkit-scrollbar {
+  width: 12px;
+  /* width of the entire scrollbar */
+}
+
+body::-webkit-scrollbar-track {
+  background: #000;
+  /* color of the tracking area */
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: #1ED760;
+  /* color of the scroll thumb */
+  border-radius: 20px;
+  /* roundness of the scroll thumb */
+  border: 3px solid #000;
+  /* creates padding around scroll thumb */
+}</style>
--- src/components/Cadastro.vue
+++ src/components/Cadastro.vue
@@ -1,19 +1,82 @@
 <template>
-    <div class="cadastro">
-      <navbar-index />
-      <h1>Cadastro</h1>
+  <div class="login">
+    <navbar-index />
+    <div class="login d-flex justify-content-center">
+      <div class="card mx-auto mt-5 mb-5">
+        <div class="card-body">
+          <h3 class="card-title mb-5">Faça seu cadastro agora mesmo!</h3>
+          <div class="container">
+            <div class="row">
+              <form>
+                <div class="form-group">
+                  <label for="email">Nome <span class="text-danger">*</span> </label>
+                  <input type="text" ref="nome" placeholder="Nome" id="nome" class="form-control mb-3 white-text"
+                    name="nome" />
+                </div>
+
+                <div class="form-group">
+                  <label for="email">E-mail <span class="text-danger">*</span> </label>
+                  <input type="text" ref="email" placeholder="e-mail" id="email" class="form-control mb-3 white-text"
+                    name="email" />
+                </div>
+
+                <div class="form-group">
+                  <label for="cpf">CPF <span class="text-danger">*</span> </label>
+                  <input type="text" ref="cpf" placeholder="CPF" id="cpf" class="form-control mb-3 white-text"
+                    name="cpf" />
+                </div>
+
+                <div class="form-group">
+                  <label for="senha">Senha <span class="text-danger">*</span></label>
+                  <div class="input-group mb-3">
+                    <input :type="mostrar_senha ? 'text' : 'password'" class="form-control white-text" placeholder="Senha"
+                      name="senha" aria-label="Senha" aria-describedby="button-addon2">
+                    <button @click="alternarExibicaoSenha()" class="btn btn-outline-success" type="button" id="senha">
+                      <i class="fa-solid" :class="{ 'fa-eye-slash': mostrar_senha, 'fa-eye': !mostrar_senha }"></i>
+                    </button>
+                  </div>
+                </div>
+
+                <div class="form-row mt-4 mb-4 text-center">
+                  <div class="form-group col-md-12 d-flex justify-content-end">
+                    <button class="btn btn-success mt-3">Fazer cadastro</button>
+                  </div>
+                </div>
+
+                <div class="form-row mt-4 text-center">
+                  <small>Já é cadastrado? <router-link class="text-success" to="/login">Faça login</router-link>
+                    agora mesmo!</small>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script lang="ts">
-  import { Options, Vue } from 'vue-class-component'
-  import NavbarIndex from '@/components/NavbarIndex.vue'
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import NavbarIndex from '@/components/NavbarIndex.vue'
+
+@Options({
+  components: {
+    NavbarIndex
+  },
+})
+export default class Login extends Vue {
+
+  mostrar_senha = false
+  senha = ''
+
+  public alternarExibicaoSenha() {
+    this.mostrar_senha = !this.mostrar_senha
+  }
+
+}
+</script>
   
-  @Options({
-    components: {
-      NavbarIndex
-    },
-  })
-  export default class Cadastro extends Vue {}
-  </script>
-  
+<style lang="scss">
+@import '../scss/forms.scss';
+</style>
